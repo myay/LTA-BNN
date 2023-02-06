dab1d05a95be8a3b10e3450e126ff684c4a51ec5
Mikail <mikail.yayla@tu-dortmund.de>
2023-02-06 13:52:39 +0100

    Comment and clean up run.py

--- run.py
+++ run.py
@@ -31,23 +31,25 @@ from BNNModels import BNN_VGG3, BNN_VGG3_TLUTRAIN, BNN_VGG7, BNN_VGG7_TLUTRAIN
 # from resnet_bnn import ResNet, BasicBlock
 
 def main():
-    # Training settings
+    # Parse args from command line
     parser = argparse.ArgumentParser(description='Training Process')
     parse_args(parser)
     args = parser.parse_args()
+
+    # GPU settings
     use_cuda = not args.no_cuda and torch.cuda.is_available()
-    torch.manual_seed(args.seed)
-
     device = torch.device("cuda" if use_cuda else "cpu")
-
     available_gpus = [i for i in range(torch.cuda.device_count())]
     print("Available GPUs: ", available_gpus)
+    # Use selected GPU
     gpu_select = args.gpu_num
-    # use selected GPU
     torch.cuda.set_device(gpu_select)
-    # GPU that is currently used
     print("Currently used GPU: ", torch.cuda.current_device())
 
+    # Seed for pytorch
+    torch.manual_seed(args.seed)
+
+    # Train and test dataset configs
     train_kwargs = {'batch_size': args.batch_size}
     test_kwargs = {'batch_size': args.test_batch_size}
     if use_cuda:
@@ -57,76 +59,79 @@ def main():
         train_kwargs.update(cuda_kwargs)
         test_kwargs.update(cuda_kwargs)
 
+    # Get model and dataset
     nn_model, dataset1, dataset2 = get_model_and_datasets(args)
-
     model = None
+    model = nn_model().to(device)
     # LTA processing cannot be applied to skip connections
     # if args.model == "ResNet18":
     #     # TODO: change model initialization
     #     model = nn_model(BasicBlock, [2, 2, 2, 2]).to(device)
     # else:
-    model = nn_model().to(device)
 
-    # set error model
+    # Set error model
     if args.error_prob is not None:
         model.error_model.p = args.error_prob
 
+    # Load dataset
     train_loader = torch.utils.data.DataLoader(dataset1,**train_kwargs)
     test_loader = torch.utils.data.DataLoader(dataset2, **test_kwargs)
 
+    # Print XNOR gates and error rate
     current_xc = args.nr_xnor_gates
     print("\n--- XNOR GATES: ", current_xc)
     if args.error_prob is not None:
         print("--- ERROR RATE: ", args.error_prob)
 
-    # nr of xnor gates = 1 is reserved for "no lta computations"
+    # Nr of xnor gates = 1 is reserved for "no lta computations"
     if current_xc != 1:
         model.lta_mode = args.lta_mode
         for layer in model.children():
             if isinstance(layer, (QuantizedConv2d, QuantizedLinear)):
                 layer.nr_xnor_gates = current_xc
 
-    # create experiment folder and file
+    # Experiment folder and file, currently disabled
     # to_dump_path = create_exp_folder(model)
     # if not os.path.exists(to_dump_path):
     #     open(to_dump_path, 'w').close()
 
+    # Optimizer and scheduler
     # optimizer = optim.Adam(model.parameters(), lr=args.lr)
     optimizer = Clippy(model.parameters(), lr=args.lr)
-
     scheduler = StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
 
-    # load training state or create new model
+    # Load training state if specified
     if args.load_training_state is not None:
         print("Loaded training state: ", args.load_training_state)
         checkpoint = torch.load(args.load_training_state)
         model.load_state_dict(checkpoint['model_state_dict'])
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
+    # Variables for time measurements
     time_elapsed = 0
     times = []
-    # taining loop
+
+    # Training loop
     if args.train_model is not None:
         for epoch in range(1, args.epochs + 1):
             start = torch.cuda.Event(enable_timing=True)
             end = torch.cuda.Event(enable_timing=True)
             start.record()
+            # Training function
             train(args, model, device, train_loader, optimizer, epoch)
             end.record()
             torch.cuda.synchronize()
             print("Run time (ms):", start.elapsed_time(end))
             times.append(start.elapsed_time(end))
+            # Testing function
             test(model, device, test_loader)
             scheduler.step()
 
-    if args.test_error:
-        all_accuracies = test_error(model, device, test_loader)
-        to_dump_data = dump_exp_data(model, args, all_accuracies)
-        store_exp_data(to_dump_path, to_dump_data)
-
+    # Save model (only for inference after loading again)
     if args.save_model is not None:
         torch.save(model.state_dict(), "model_{}_{}.pt".format(args.save_model, current_xc))
 
+    # Save training state for retraining
     if args.save_training_state is not None:
         path = "model_checkpoint_{}.pt".format(args.save_training_state)
         torch.save({
@@ -136,12 +141,14 @@ def main():
         }, path)
         print("Training state saved.")
 
+    # Load model file to test it
     if args.load_model_path is not None:
         to_load = args.load_model_path
         print("Loaded model: ", to_load)
         model.load_state_dict(torch.load(to_load, map_location='cuda:0'))
         test(model, device, test_loader)
 
+    # Print the measured times
     if args.profile_time is not None:
         print_tikz_data(times)
 
